f316aa14e020e49de2881571f4025e75ad1627f5
Bernhard Mayer <bernhard@generationmake.de>
2021-11-08 22:36:04 +0100

    added check for file format

--- converter.py
+++ converter.py
@@ -1,8 +1,11 @@
 import sys
+import os
 
 if len(sys.argv) < 2:
   print("add file name")
   exit()
+filename=os.path.splitext(sys.argv[1])[0]
+filetype=os.path.splitext(sys.argv[1])[1].lower()
 
 # Using readlines()
 file1 = open(sys.argv[1], 'r')
@@ -13,62 +16,68 @@ mode = 0
 header = {}
 qsos = []
 countqso = 0
+if filetype==".stf":
+  print("STF format")
 # Strips the newline character
-for line in Lines:
-  if count == 0:
-    if line.strip()=="STF1":
-      print("format OK")
-    else:
-      print("wrong file format")
-      exit()
-  if mode == 0:
-    if line.strip()=="HEADER":
-      mode = 1
-    if line.strip()=="QSOLIST":
-      mode = 2
+  for line in Lines:
+    if count == 0:
+      if line.strip()=="STF1":
+        print("format OK")
+      else:
+        print("wrong file format")
+        exit()
+    if mode == 0:
+      if line.strip()=="HEADER":
+        mode = 1
+      if line.strip()=="QSOLIST":
+        mode = 2
 
-  if mode == 1:  # HEADER
-    if line.strip()=="ENDHEADER":
-      mode = 0
-    key=line.strip().split()
-    value=line[len(key[0]):].strip()
-    if key[0]=="QSOORDER":
-      print("qsoorder:" + value)
-      qsokeys=value.split()
-    elif key[0]=="HEADER":
-      print("header")
-    elif key[0]=="ENDHEADER":
-      print("endheader")
-    else:
-#      print("key: {}, value: {}".format(key[0], value))
-      oldvalue=header.pop(key[0],False)
-      if oldvalue:
-        value = oldvalue + " " + value
-      header.update({key[0]:value})
+    if mode == 1:  # HEADER
+      if line.strip()=="ENDHEADER":
+        mode = 0
+      key=line.strip().split()
+      value=line[len(key[0]):].strip()
+      if key[0]=="QSOORDER":
+        print("qsoorder:" + value)
+        qsokeys=value.split()
+      elif key[0]=="HEADER":
+        print("header")
+      elif key[0]=="ENDHEADER":
+        print("endheader")
+      else:
+#        print("key: {}, value: {}".format(key[0], value))
+        oldvalue=header.pop(key[0],False)
+        if oldvalue:
+          value = oldvalue + " " + value
+        header.update({key[0]:value})
 
-  if mode == 2:  # QSOLIST
-    if line.strip()=="ENDQSOLIST":
-      mode = 0
-    elem=line.strip().split()
-    if elem[0]=="QSOLIST":
-      print("qsolist")
-    elif elem[0]=="ENDQSOLIST":
-      print("endqsolist")
-    else:
-#      print(qsokeys)
-      print(elem)
-      qsos.append(elem)
-      countqso += 1
+    if mode == 2:  # QSOLIST
+      if line.strip()=="ENDQSOLIST":
+        mode = 0
+      elem=line.strip().split()
+      if elem[0]=="QSOLIST":
+        print("qsolist")
+      elif elem[0]=="ENDQSOLIST":
+        print("endqsolist")
+      else:
+#        print(qsokeys)
+        print(elem)
+        qsos.append(elem)
+        countqso += 1
 
 #  if mode != 1:  
-  if mode == 0:  
-    print("Line{}-Mode{}: {}".format(count, mode, line.strip()))
-  count += 1
+    if mode == 0:  
+      print("Line{}-Mode{}: {}".format(count, mode, line.strip()))
+    count += 1
+else:
+  print("wrong format!!!")
+  exit()
 
+#generate adif file
 #print(header)
 #print(qsos)
 #write content to new file
-logfilename = (sys.argv[1].split('.'))[0] + ".adi"
+logfilename = filename + ".adi"
 #f = open("log.adi", "w")
 f = open(logfilename, "w")
 f.write("generated on 2021-11-05 for {}\n\n".format(header.get("MYCALL")));
